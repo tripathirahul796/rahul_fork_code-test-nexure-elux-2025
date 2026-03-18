7d8d6eb27d87aec5cd9dd28dc8458c26549aa31b
Rahul Tripathi <tripathirahul796@gmail.com>
2026-03-18 13:33:56 +0530

    Fix concurrency issue: use atomic MongoDB $addToSet operation

--- discount/app/src/main/kotlin/io/nexure/discount/repository/ProductRepository.kt
+++ discount/app/src/main/kotlin/io/nexure/discount/repository/ProductRepository.kt
@@ -1,8 +1,11 @@
 package io.nexure.discount.repository
 
 import com.mongodb.client.model.Filters
+import com.mongodb.client.model.FindOneAndUpdateOptions
 import com.mongodb.client.model.IndexOptions
 import com.mongodb.client.model.Indexes
+import com.mongodb.client.model.ReturnDocument
+import com.mongodb.client.model.Updates
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import io.nexure.discount.model.Discount
@@ -45,20 +48,14 @@ class ProductRepository(mongoClient: MongoClient, databaseName: String = "produc
     }
     
     suspend fun applyDiscount(productId: String, discount: Discount): Product? {
-        val product = findById(productId) ?: return null
+        val filter = Filters.eq("id", productId)
+        val updated = collection.findOneAndUpdate(
+            filter,
+            Updates.addToSet("discounts", discount),
+            FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER)
+        )
         
-        val hasDiscount = product.discounts.any { it.discountId == discount.discountId }
-        if (hasDiscount) {
-            return product
-        }
-        
-        // Throttle to prevent MongoDB write overload
-        kotlinx.coroutines.delay(5)
-        
-        val updatedDiscounts = product.discounts + discount
-        val updatedProduct = product.copy(discounts = updatedDiscounts)
-        
-        return save(updatedProduct)
+        return updated
     }
     
     suspend fun deleteAll() {
